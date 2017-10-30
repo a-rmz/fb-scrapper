2fe0deb5bb67b6c04a941f3296629289c58b0377
Alejandro Ramirez <armzprz@gmail.com>
2017-10-30 10:57:52 +0200

    Add post scrapping
    Also, some pretty prints are used to log the status of the query.

--- app/fbpostscrapper.rb
+++ app/fbpostscrapper.rb
@@ -11,7 +11,7 @@ module Fbpostscrapper
   @scrapper = Scrapper.new()
   @storage = Storage.new()
 
-  page_list = ['MiguelOsorioChong',
+  @page_list = ['MiguelOsorioChong',
     'SilvanoAureoles',
     'MargaritaZavalaMX',
     'emilioalvarezicaza',
@@ -21,7 +21,7 @@ module Fbpostscrapper
     'lopezobrador.org.mx'
   ]
 
-  page_list.each do |page_id|
+  @page_list.each do |page_id|
     scrapped_page = @scrapper.scrape_page(page_id)
 
     page = Page.new(
@@ -31,13 +31,25 @@ module Fbpostscrapper
       scrapped_page['fan_count']
     )
 
-#     @storage.insert_page(page)
+    page_result = @storage.insert_page(page)
+    if page_result['error'] then
+      puts 'Page retreived successfully'
+    elsif page_result['skipped'] then
+      puts 'Page skipped'
+    else
+      puts 'Error while inserting page'
+    end
+   
+    puts "Scrapping: #{page.name}"
+    since = Time.new("2017", "10", "20")
+    posts = @scrapper.scrape_posts(page.slug_id, since)
 
-    posts = @scrapper.scrape_posts(page.slug_id, Time.new("2017", "10", "16"))
+    results = { :success=>0, :error=>0, :skipped=>0 }
     posts.each do |post|
-
       id = post['id']
       message = post['message']
+      story = post['story']
+      link = post['link']
       created_time = post['created_time']
       like = post['like']['summary']['total_count']
       love = post['love']['summary']['total_count']
@@ -45,13 +57,27 @@ module Fbpostscrapper
       haha = post['haha']['summary']['total_count']
       sad = post['sad']['summary']['total_count']
       angry = post['angry']['summary']['total_count']
+      thankful = post['thankful']['summary']['total_count']
+      pride = post['pride']['summary']['total_count']
 
-      reactions = Reactions.new(like, love, wow, haha, sad, angry)
-      post_obj = Post.new(id, page.id, message, created_time, reactions)
-      @storage.insert_post(post_obj)
+      reactions = Reactions.new(like, love, wow, haha, sad, angry, thankful, pride)
+      post_obj = Post.new(id, page.id, message, story, link, created_time, reactions)
+      
+      result = @storage.insert_post(post_obj)
+      if result[:error] then
+        results[:error] += 1
+      elsif result[:skipped] then
+        results[:skipped] += 1
+      else
+        results[:success] += 1
+      end
     end
-  puts  ""
-  puts  ""
+  
+    puts "#{results[:success]} posts inserted successfully"
+    puts "#{results[:skipped]} posts skipped"
+    puts "#{results[:error]} errors"
+    puts  ""
+    puts  ""
   end # end do
 
 end
