e91477d497b88ed72fd5fa01f250fffaf11361de
Alejandro Ramirez <armzprz@gmail.com>
2017-11-03 12:17:26 +0200

    Comment scrapping and logging

--- app/fbpostscrapper.rb
+++ app/fbpostscrapper.rb
@@ -1,25 +1,35 @@
+require 'logger'
+require 'time'
+require 'time_difference'
 require 'yaml'
+
 require_relative 'scrapper'
 require_relative 'storage' 
 
+require_relative 'schema/comment'
 require_relative 'schema/page'
 require_relative 'schema/post'
 require_relative 'schema/fan_count'
 
 module Fbpostscrapper
 
+  log_date = Time.now.strftime "%Y-%m-%d_%H:%M:%S"
+  @logger = Logger.new("log/#{log_date}.log")
+
   @scrapper = Scrapper.new()
   @storage = Storage.new()
 
-  @page_list = ['MiguelOsorioChong',
-    'SilvanoAureoles',
-    'MargaritaZavalaMX',
-    'emilioalvarezicaza',
-    'JaimeRodriguezElBronco',
-    'JoseAMeadeK',
-    'MiguelAngelMancera',
-    'lopezobrador.org.mx'
-  ]
+  @page_list = YAML.load_file('page_list.yaml')
+  @scrapper_config = YAML.load_file('config/scrapper.yaml')
+
+  @since = Time.parse(@scrapper_config['date_range']['since'])
+  @start_time = Time.now
+
+  @logger.info "Starting job at #{@start_time.inspect}"
+  @logger.info ""
+  @logger.info "Scrapping posts since #{@since.asctime}"
+  @logger.info ""
+  @logger.info ""
 
   @page_list.each do |page_id|
     scrapped_page = @scrapper.scrape_page(page_id)
@@ -31,20 +41,22 @@ module Fbpostscrapper
       scrapped_page['fan_count']
     )
 
+    @logger.info "Scrapping: #{page.name}"
     page_result = @storage.insert_page(page)
-    if page_result['error'] then
-      puts 'Page retreived successfully'
-    elsif page_result['skipped'] then
-      puts 'Page skipped'
+    if page_result['inserted'] == 1 then
+      @logger.info 'Page inserted successfully'
+    elsif page_result['skipped'] == 1 then
+      @logger.info 'Page skipped'
+    elsif page_result['replaced'] == 1 then
+      @logger.info 'Page updated'
     else
-      puts 'Error while inserting page'
+      @logger.info 'Error while inserting page'
     end
    
-    puts "Scrapping: #{page.name}"
-    since = Time.new("2017", "10", "20")
-    posts = @scrapper.scrape_posts(page.slug_id, since)
+    posts = @scrapper.scrape_posts(page.slug_id, @since)
 
-    results = { :success=>0, :error=>0, :skipped=>0 }
+    post_results = { :success=>0, :error=>0, :skipped=>0 }
+    comment_results = { :success=>0, :error=>0, :skipped=>0 }
     posts.each do |post|
       id = post['id']
       message = post['message']
@@ -64,20 +76,67 @@ module Fbpostscrapper
       post_obj = Post.new(id, page.id, message, story, link, created_time, reactions)
       
       result = @storage.insert_post(post_obj)
-      if result[:error] then
-        results[:error] += 1
-      elsif result[:skipped] then
-        results[:skipped] += 1
+
+      if result['errors'] == 1 then
+        post_results[:error] += 1
+      elsif result['skipped'] == 1 then
+        post_results[:skipped] += 1
       else
-        results[:success] += 1
+        post_results[:success] += 1
       end
-    end
+
+      # Scrape the comments!
+      comments = @scrapper.scrape_comments(post_obj.id)
+
+      comments_processed = 0
+
+      comments.each do |comment|
+        id = comment['id']
+        message = comment['message']
+        created_time = comment['created_time']
+        from = comment['from']
+        like = comment['like']['summary']['total_count']
+        love = comment['love']['summary']['total_count']
+        wow = comment['wow']['summary']['total_count']
+        haha = comment['haha']['summary']['total_count']
+        sad = comment['sad']['summary']['total_count']
+        angry = comment['angry']['summary']['total_count']
+        thankful = comment['thankful']['summary']['total_count']
+        pride = comment['pride']['summary']['total_count']
+
+        reactions = Reactions.new(like, love, wow, haha, sad, angry, thankful, pride)
+        comment_obj = Comment.new(id, post_obj.id, message, created_time, from, reactions, [])
+       
+        result = @storage.insert_comment(comment_obj)
+        comments_processed += 1
+        print "#{comments_processed} comments processed\r"
+        $stdout.flush
+
+        if result['errors'] == 1 then
+          comment_results[:error] += 1
+        elsif result['skipped'] == 1 then
+          comment_results[:skipped] += 1
+        else
+          comment_results[:success] += 1
+        end
+      end # end comments do
+
+    end # end posts do
   
-    puts "#{results[:success]} posts inserted successfully"
-    puts "#{results[:skipped]} posts skipped"
-    puts "#{results[:error]} errors"
-    puts  ""
-    puts  ""
-  end # end do
+    @logger.info "#{post_results[:success]} posts inserted successfully"
+    @logger.info "#{post_results[:skipped]} posts skipped"
+    @logger.info "#{post_results[:error]} errors"
+    @logger.info  ""
+    @logger.info "#{comment_results[:success]} comments inserted successfully"
+    @logger.info "#{comment_results[:skipped]} comments skipped"
+    @logger.info "#{comment_results[:error]} errors"
+    @logger.info  ""
+    @logger.info  ""
+  end # end pages do
+
+  @end_time = Time.now
+  @time_difference = TimeDifference.between(@start_time, @end_time)
+  @logger.info "Ending job at #{@end_time.inspect}"
+  @logger.info "Time elapsed: #{@time_difference.humanize}"
 
 end
